b2a18520fccf3af6c04a3fe1ae556d04e40fd74c
Darwing27 <dsrondon96@misena.edu.co>
2024-09-11 10:51:01 -0500

    ajuste

--- app/Http/Controllers/API/IpsController.php
+++ app/Http/Controllers/API/IpsController.php
@@ -71,7 +71,7 @@ class IpsController extends Controller
                 $admin->cod_ips = $ips->cod_ips; // Asociar el admin a la IPS recién creada
                 $admin->nom_admin = $request->nom_ips;
                 $admin->ape_admin = ' ';
-                $admin->tipo_documento = 10;
+                $admin->cod_documento = 10;
                 $admin->email_admin = $request->email_ips;
                 $admin->tel_admin = $request->tel_ips;
                 $admin->documento_admin = $request->nit_ips;
